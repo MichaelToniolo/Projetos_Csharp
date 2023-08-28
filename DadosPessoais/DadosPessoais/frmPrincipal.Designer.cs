ea9d7d8a6b819acd5c63851cdf96e15ff48f3640
Michael (a.k.a) Mz3ro <37878223+MichaelToniolo@users.noreply.github.com>
2023-08-28 14:31:12 -0300

    Dados Pessoais

---
+++ DadosPessoais/DadosPessoais/frmPrincipal.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace DadosPessoais
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.gpbSexo = new System.Windows.Forms.GroupBox();
+            this.rdbF = new System.Windows.Forms.RadioButton();
+            this.rdbM = new System.Windows.Forms.RadioButton();
+            this.dtpData = new System.Windows.Forms.DateTimePicker();
+            this.dtNasc = new System.Windows.Forms.Label();
+            this.lblUF = new System.Windows.Forms.Label();
+            this.cbbUF = new System.Windows.Forms.ComboBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnConcatenar = new System.Windows.Forms.Button();
+            this.gpbSexo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(63, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(159, 47);
+            this.txtNome.MaxLength = 50;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(312, 20);
+            this.txtNome.TabIndex = 1;
+            this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
+            // 
+            // gpbSexo
+            // 
+            this.gpbSexo.Controls.Add(this.rdbF);
+            this.gpbSexo.Controls.Add(this.rdbM);
+            this.gpbSexo.Location = new System.Drawing.Point(428, 97);
+            this.gpbSexo.Name = "gpbSexo";
+            this.gpbSexo.Size = new System.Drawing.Size(119, 59);
+            this.gpbSexo.TabIndex = 2;
+            this.gpbSexo.TabStop = false;
+            this.gpbSexo.Text = "Sexo";
+            // 
+            // rdbF
+            // 
+            this.rdbF.AutoSize = true;
+            this.rdbF.Location = new System.Drawing.Point(73, 19);
+            this.rdbF.Name = "rdbF";
+            this.rdbF.Size = new System.Drawing.Size(31, 17);
+            this.rdbF.TabIndex = 1;
+            this.rdbF.TabStop = true;
+            this.rdbF.Text = "F";
+            this.rdbF.UseVisualStyleBackColor = true;
+            // 
+            // rdbM
+            // 
+            this.rdbM.AutoSize = true;
+            this.rdbM.Location = new System.Drawing.Point(6, 19);
+            this.rdbM.Name = "rdbM";
+            this.rdbM.Size = new System.Drawing.Size(34, 17);
+            this.rdbM.TabIndex = 0;
+            this.rdbM.TabStop = true;
+            this.rdbM.Text = "M";
+            this.rdbM.UseVisualStyleBackColor = true;
+            // 
+            // dtpData
+            // 
+            this.dtpData.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpData.Location = new System.Drawing.Point(159, 97);
+            this.dtpData.Name = "dtpData";
+            this.dtpData.Size = new System.Drawing.Size(99, 20);
+            this.dtpData.TabIndex = 3;
+            // 
+            // dtNasc
+            // 
+            this.dtNasc.AutoSize = true;
+            this.dtNasc.Location = new System.Drawing.Point(38, 97);
+            this.dtNasc.Name = "dtNasc";
+            this.dtNasc.Size = new System.Drawing.Size(107, 13);
+            this.dtNasc.TabIndex = 4;
+            this.dtNasc.Text = "Data de Nascimento:";
+            // 
+            // lblUF
+            // 
+            this.lblUF.AutoSize = true;
+            this.lblUF.Location = new System.Drawing.Point(99, 143);
+            this.lblUF.Name = "lblUF";
+            this.lblUF.Size = new System.Drawing.Size(24, 13);
+            this.lblUF.TabIndex = 5;
+            this.lblUF.Text = "UF:";
+            // 
+            // cbbUF
+            // 
+            this.cbbUF.FormattingEnabled = true;
+            this.cbbUF.Items.AddRange(new object[] {
+            "AC",
+            "AL",
+            "AM",
+            "AP",
+            "BA",
+            "CE",
+            "DF",
+            "ES",
+            "GO",
+            "MA",
+            "MG",
+            "MS",
+            "MT",
+            "PA",
+            "PB",
+            "PE",
+            "PI",
+            "PR",
+            "RJ",
+            "RN",
+            "RO",
+            "RR",
+            "RS",
+            "SC",
+            "SE",
+            "SP",
+            "TO"});
+            this.cbbUF.Location = new System.Drawing.Point(159, 135);
+            this.cbbUF.Name = "cbbUF";
+            this.cbbUF.Size = new System.Drawing.Size(50, 21);
+            this.cbbUF.Sorted = true;
+            this.cbbUF.TabIndex = 6;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultado.Location = new System.Drawing.Point(41, 206);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(330, 115);
+            this.lblResultado.TabIndex = 7;
+            // 
+            // btnConcatenar
+            // 
+            this.btnConcatenar.BackgroundImage = global::DadosPessoais.Properties.Resources.Sem_títuloasuahdjas;
+            this.btnConcatenar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnConcatenar.Location = new System.Drawing.Point(410, 206);
+            this.btnConcatenar.Name = "btnConcatenar";
+            this.btnConcatenar.Size = new System.Drawing.Size(122, 112);
+            this.btnConcatenar.TabIndex = 8;
+            this.btnConcatenar.UseVisualStyleBackColor = true;
+            this.btnConcatenar.Click += new System.EventHandler(this.btnConcatenar_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 330);
+            this.Controls.Add(this.btnConcatenar);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.cbbUF);
+            this.Controls.Add(this.lblUF);
+            this.Controls.Add(this.dtNasc);
+            this.Controls.Add(this.dtpData);
+            this.Controls.Add(this.gpbSexo);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Name = "frmPrincipal";
+            this.Text = "Form1";
+            this.gpbSexo.ResumeLayout(false);
+            this.gpbSexo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.GroupBox gpbSexo;
+        private System.Windows.Forms.RadioButton rdbF;
+        private System.Windows.Forms.RadioButton rdbM;
+        private System.Windows.Forms.DateTimePicker dtpData;
+        private System.Windows.Forms.Label dtNasc;
+        private System.Windows.Forms.Label lblUF;
+        private System.Windows.Forms.ComboBox cbbUF;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnConcatenar;
+    }
+}
+
---
+++ DadosPessoais/DadosPessoais/frmPrincipal.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DadosPessoais
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        #region LetrasMaiúsculas
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+            txtNome.CharacterCasing = CharacterCasing.Upper;
+        }
+        #endregion
+
+        #region
+        private void btnConcatenar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string nome = txtNome.Text;
+                //validar o nome
+                if (nome.Length == 0)
+                {
+                    txtNome.Focus();
+                    throw new Exception("É necessário preecher nome");
+                }
+
+                DateTime dtnasc = dtpData.Value;
+                string uf = cbbUF.Text;
+
+                //validar estado
+                
+                if (uf.Length == 0 )
+                {
+                    throw new Exception("Favor preencher o Estado (UF)");
+                }
+                string sexo;
+
+                //validar sexo
+                if(rdbM.Checked == false && rdbF.Checked == false)
+                {
+                    throw new Exception("Favor preencher o sexo");
+                }
+
+                else if (rdbM.Checked == true)
+                {
+                    sexo = "Masculino";
+                }
+                else
+                {
+                    sexo = "Feminino";
+                }
+
+                lblResultado.Text = nome + " nasceu em " + Convert.ToString(dtNasc) + " reside no estado de " + uf + ", e é do sexo " + sexo;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        #endregion
+    }
+}
